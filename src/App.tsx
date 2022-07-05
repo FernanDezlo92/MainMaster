e367db372d83494d983842e5f093b7f0e5c27006
FernanDezlo93 <94480359+FernanDezlo93@users.noreply.github.com>
2022-07-05 12:19:32 +0200

    Sin cambios

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,14 @@ import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+ const [count, setCount] = useState(0)
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React! TUPAC</p>
         <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
+          <button type="button" onClick={() => setCount ((count)=> count +1)}>
             count is: {count}
           </button>
         </p>
